6614e921dd454c383b72e5198343a7b7cc4cc505
Fan Yang <missdeer@dfordsoft.com>
2015-03-21 15:15:13 +0800

    Update ladder.sh

--- ladder.sh
+++ ladder.sh
@@ -72,7 +72,7 @@ chmod a+x ./start.sh
 wget -t0 -T10 -O ./stop.sh https://raw.githubusercontent.com/missdeer/ladder/master/stop.sh
 chmod a+x ./stop.sh
 bin=cow-$os$arch-$version
-binary_url="http://hp.codefast.tk/cow/$bin.$postfix"
+binary_url="http://dl.chenyufei.info/cow/$bin.$postfix"
 if [[ "$os" == "win" ]]
 then
     #haproxy -f ./haproxy.cfg -p ./haproxy.pid
